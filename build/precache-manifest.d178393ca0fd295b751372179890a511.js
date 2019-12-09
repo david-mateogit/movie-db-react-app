0dc9909694ffb78f298b1fad8dd10a9690c91d17
david-mateogit <davidmateo@outlook.com>
2019-12-08 20:06:11 -0400

    deploy Netlify

---
+++ build/precache-manifest.d178393ca0fd295b751372179890a511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b44b3df864069e77672e66376fea19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d8294b66e7606a28eda",
+    "url": "/static/css/main.c4b14704.chunk.css"
+  },
+  {
+    "revision": "4b8fbcdc80ee291d23fc",
+    "url": "/static/js/2.60295ff1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.60295ff1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d8294b66e7606a28eda",
+    "url": "/static/js/main.c2b4fee7.chunk.js"
+  },
+  {
+    "revision": "9bfd7cbee2d5ad26671a",
+    "url": "/static/js/runtime-main.e580f368.js"
+  }
+]);
