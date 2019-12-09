c742d74733a53c4dd1487a17b5e52c0eb038140c
david-mateogit <davidmateo@outlook.com>
2019-12-09 09:43:33 -0400

    css fixes youtube embed

--- src/components/elements/MovieTrailers/MovieTrailers.js
+++ src/components/elements/MovieTrailers/MovieTrailers.js
@@ -7,28 +7,29 @@ const MovieTrailers = ({ trailers }) => {
   return (
     <div className="rmdb-movietrailer">
       <div className="rmdb-movietrailer-content">
-        <div className="rmdb-movietrailer-content-col">
-          {trailers[0] && <Youtube id={trailers[0] && trailers[0].trailer} />}
-          <span className="rmdb-movietrailer-info" />
-        </div>
-        <div className="rmdb-movietrailer-content-col">
-          {trailers[1] && <Youtube id={trailers[1] && trailers[1].trailer} />}
-          <span className="rmdb-movietrailer-info" />
-        </div>
-        <div className="rmdb-movietrailer-content-col">
-          {trailers[2] && <Youtube id={trailers[2] && trailers[2].trailer} />}
-          <span className="rmdb-movietrailer-info" />
-        </div>
+        {trailers[0] && (
+          <div className="rmdb-movietrailer-content-col">
+            <Youtube id={trailers[0] && trailers[0].trailer} />
+          </div>
+        )}
+
+        {trailers[1] && (
+          <div className="rmdb-movietrailer-content-col">
+            <Youtube id={trailers[1] && trailers[1].trailer} />
+          </div>
+        )}
+
+        {trailers[2] && (
+          <div className="rmdb-movietrailer-content-col">
+            <Youtube id={trailers[2] && trailers[2].trailer} />
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-// MovieTrailers.propTypes = {
-//   movie: PropTypes.shape({
-//     runtime: PropTypes.number.isRequired,
-//     budget: PropTypes.number.isRequired,
-//     revenue: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
+MovieTrailers.propTypes = {
+  trailers: PropTypes.array.isRequired,
+};
 export default MovieTrailers;
